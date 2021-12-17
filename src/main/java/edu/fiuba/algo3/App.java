01095bd159a09b30c640e94dc8db7ec659c63780
kamalhosain <khosain@fi.uba.ar>
2021-12-17 15:17:35 -0300

    interfaz basica

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -1,10 +1,15 @@
 package edu.fiuba.algo3;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
 /**
  * JavaFX App
@@ -14,10 +19,38 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+        stage.setTitle("Dónde $@#! está Carmen Sandiego?");
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        Label labelNombreJuego = new Label("Where in the World is Carmen Sandiego? // Grupo 4");
+        Label labelVersion = new Label("running on Java " + javaVersion + ".");
+
+        HBox layoutHB = new HBox();
+        Label labelNombreJugador = new Label("Nombre del jugador: ");
+        TextField nombreJugadorTF = new TextField();
+        nombreJugadorTF.setPromptText("Ingrese su nombre");
+        layoutHB.getChildren().add(labelNombreJugador);
+        layoutHB.getChildren().add(nombreJugadorTF);
+
+        Button botonEnviar = new Button("Enviar");
+        Label etiqueta = new Label();
+
+        BotonEnviarEventHandler botonEnviarEventHandler = new BotonEnviarEventHandler(nombreJugadorTF,etiqueta);
+        botonEnviar.setOnAction(botonEnviarEventHandler);
+
+
+        TextoEventHandler textoEventHandler = new TextoEventHandler(botonEnviar);
+        nombreJugadorTF.setOnKeyPressed(textoEventHandler);
+
+        VBox layout = new VBox(labelNombreJuego, labelVersion);
+        layout.getChildren().add(layoutHB);
+        layout.getChildren().add(botonEnviar);
+        layout.getChildren().add(etiqueta);
+        layout.setSpacing(15);
+        layout.setPadding(new Insets(20));
+
+
+
+        var scene = new Scene(layout, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/edu/fiuba/algo3/TextoEventHandler.java
@@ -0,0 +1,27 @@
+package edu.fiuba.algo3;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public class TextoEventHandler implements EventHandler<KeyEvent> {
+
+    private Button botonEnviar;
+
+    public TextoEventHandler(Button botonEnviar){
+        this.botonEnviar = botonEnviar;
+    }
+
+    @Override
+    public void handle(KeyEvent keyEvent){
+
+        System.out.println(keyEvent.getCode());
+
+        if(keyEvent.getCode() == KeyCode.ENTER){
+            Event.fireEvent(botonEnviar, new ActionEvent());
+        }
+    }
+}
