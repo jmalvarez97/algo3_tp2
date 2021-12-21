04a5f84d99206d273b013d5a7d4db927a2d5501b
Manuel Alvarez <jualvarez@fi.uba.ar>
2021-12-20 22:15:07 -0300

    Edit decorator

---
+++ src/main/java/edu/fiuba/algo3/Decorator/Senia.java
@@ -0,0 +1,17 @@
+package edu.fiuba.algo3.Decorator;
+
+import java.util.Objects;
+
+public class Senia extends Decorator{
+    public Senia(Pregunta pregunta, String cualidad) {
+        super(pregunta, cualidad);
+    }
+
+    @Override
+    public boolean preguntar(String dato) {
+        if (Objects.equals(this.cualidad, dato))
+            return true;
+        return this.pregunta.preguntar(dato);
+    }
+}
+
