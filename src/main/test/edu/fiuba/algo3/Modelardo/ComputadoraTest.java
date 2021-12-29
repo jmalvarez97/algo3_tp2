83d5185e7aa2f4c5956cc981d5c916298a38415e
deve023 <adevedia@fi.uba.ar>
2021-12-29 23:49:32 +0100

    Test computadora

--- src/main/test/edu/fiuba/algo3/Modelardo/ComputadoraTest.java
+++ src/main/test/edu/fiuba/algo3/Modelardo/ComputadoraTest.java
@@ -1,6 +1,5 @@
 package edu.fiuba.algo3.Modelardo;
 
-import edu.fiuba.algo3.Controlador.LecturaLadrones;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -9,17 +8,39 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ComputadoraTest {
+    private Ladron ladron1 = new Ladron("Castaño", "Tenis", "Descapotable Packard 1939", "Femenino", "Jamás se muestra en público sin su collar de rubíes");
+    private Ladron ladron2 = new Ladron("Rubio", "Futbol", "Descapotable Packard 1940", "Masculino", "Jamás se muestra en público sin su collar de rubíes");
+    private Ladron ladron3 = new Ladron("Castaño", "Hockey", "Descapotable Packard 1941", "Femenino", "Jamás se muestra en público sin su collar de rubíes");
+    private ArrayList<Ladron> listaLadrones = new ArrayList<Ladron>(){{add(ladron1); add(ladron2);add(ladron3);}};
+
+    private Computadora computadora = new Computadora(listaLadrones);
+
     @Test
     public void testAlCrearUnaBusquedaDeSospechososCastañosNosDevuelveLosPosiblesLadrones(){
-        ArrayList<Ladron> listaLadrones = LecturaLadrones.leerLadrones();
-        Computadora computadora = new Computadora(listaLadrones);
-
         computadora.cargarPelo("Castaño");
 
         ArrayList<Ladron> listaSospechosos = computadora.buscarSospechosos();
 
-        assertEquals(listaSospechosos.size(), 3);
+        assertEquals(listaSospechosos.size(), 2);
 
+    }
+
+    @Test
+    public void testSeChequeaOrdenDeArrestoTeniendoUnUnicoSospechosoPosibleEsValida(){
+
+        // Cargamos los datos correspondientes a 'Carmen Sandiego'
+        computadora.cargarSexo("Femenino");
+        computadora.cargarPelo("Castaño");
+        computadora.cargarHobby("Tenis");
+        computadora.cargarVehiculo("Descapotable Packard 1939");
+
+        // Debería dar un solo sospechoso y generar la orden de arresto
+        ArrayList<Ladron> listaSospechosos = computadora.buscarSospechosos();
+
+        assertEquals(listaSospechosos.size(), 1);
+
+        // La orden de arresto emitida corresponde al ladron buscado
+        assertTrue(computadora.chequearOrdenDeArresto(listaLadrones.get(0)));
     }
 
 }
